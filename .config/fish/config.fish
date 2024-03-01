5249e9469d325580b2840d5804877fa16dea617c
Bluewolf787 <dev.bluewolf@gmail.com>
2024-03-01 11:55:53 +0100

    added configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,37 @@
+# Disable greeting
+set -g fish_greeting
+
+if status is-login
+  if test (tty) = /dev/tty1
+    Hyprland
+  end
+end
+
+# Alias
+alias ls "exa --icons"
+alias la "ls -a"
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
+
+alias bon "bluetoothctl power on"
+alias boff "bluetoothctl power off"
+alias bc "bluetoothctl connect 14:3F:A6:A7:D9:81"
+alias be "systemctl enable bluetooth"
+alias bd "systemctl disable bluetooth"
+
+alias clock "tty-clock -c -C 5"
+
+alias note "nvim ~/Dokumente/notes.txt"
+
+alias update "sudo pacman -Syu && yay -Syu"
+alias autoremove "sudo pacman -Qdtq | sudo pacman -Rs -"
+alias autoclean "sudo pacman -Sc"
+
+alias nitch "~/.config/nitch/nitch"
+
+source ~/.fishmarks/marks.fish
+
+export PATH="/opt/flutter/bin/:$PATH"
+export PATH="$PATH:$HOME/.local/bin"
+export UMLET_HOME="$HOME/bin/Umlet/"
+export PATH="$PATH:$UMLET_HOME"
