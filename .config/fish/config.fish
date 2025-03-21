49fd909add984900392341ba0f73c81b5b4a3c31
Bluewolf787 <dev.bluewolf@gmail.com>
2025-03-21 19:27:55 +0100

    fix asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -198,7 +198,16 @@ source ~/.fishmarks/marks.fish
 # -----------------------------------------------------
 # asdf
 # -----------------------------------------------------
-source /opt/asdf-vm/asdf.fish
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # -----------------------------------------------------
 # ENVIRONMENT
